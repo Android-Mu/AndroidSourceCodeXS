92dfb51476d228ca9cd432fc3d265c9e63145e62
Android-Mu <1271206271@qq.com>
2017-08-04 17:03:46 +0800

    file manager path

--- OpenSysMedia/app/src/main/java/com/codexiaosheng/opensysmedia/MainActivity.java
+++ OpenSysMedia/app/src/main/java/com/codexiaosheng/opensysmedia/MainActivity.java
@@ -2,8 +2,6 @@ package com.codexiaosheng.opensysmedia;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -194,6 +192,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
                 case VIDEO_REQUEST_CODE:
                     Toast.makeText(this, "local video", Toast.LENGTH_SHORT).show();
+                    getResultPath(data);
                     break;
                 case RECORDING_VIDEO_REQUEST_CODE:
                     Toast.makeText(this, "recording video", Toast.LENGTH_SHORT).show();
@@ -209,7 +208,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
                 case FILE_MANAGER_REQUEST_CODE:
                     Toast.makeText(this, "local file", Toast.LENGTH_SHORT).show();
-                    getResultPath(data);
+                    Uri uri = data.getData();
+                    tvShowPath.setText(uri.toString());
                     break;
             }
         }
@@ -228,29 +228,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String a_path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
             tvShowPath.setText(a_path);
         }
-    }
-
-    /**
-     * 获取视频的缩略图
-     * <p>
-     * 先通过ThumbnailUtils来创建一个视频的缩略图，然后再利用ThumbnailUtils来生成指定大小的缩略图。
-     * 如果想要的缩略图的宽和高都小于MICRO_KIND，则类型要使用MICRO_KIND作为kind的值，这样会节省内存。
-     *
-     * @param videoPath 视频的路径
-     * @param width     指定输出视频缩略图的宽度
-     * @param height    指定输出视频缩略图的高度度
-     * @param kind      参照MediaStore.Images.Thumbnails类中的常量MINI_KIND和MICRO_KIND。
-     *                  其中，MINI_KIND: 512 x 384，MICRO_KIND: 96 x 96
-     * @return 指定大小的视频缩略图
-     */
-    private Bitmap getVideoThumbnail(String videoPath, int width, int height,
-                                     int kind) {
-        Bitmap bitmap = null;
-        // 获取视频的缩略图
-        bitmap = ThumbnailUtils.createVideoThumbnail(videoPath, kind);
-        bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,
-                ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
-        return bitmap;
     }
 
 }
